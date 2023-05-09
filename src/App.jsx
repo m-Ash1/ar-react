9065be53e5e39497623eabb1fc3e4e2d1c7a9804
Mahmoud Ashraf <mashraffarouk2702@gmail.com>
2023-05-09 13:03:57 +0300

    dealing with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card.jsx";
 function App() {
-  const boys = [
+  const [boyState, setState] = useState([
     { name: "Ash", level: 127, type: "Archer" },
     { name: "Fathy", level: 97, type: "Ninja" },
     { name: "Ahmed", level: 112, type: "Fighter" },
-  ];
-  const girls = [
-    { name: "Aya", level: 127, type: "Archer" },
-    { name: "Dodo", level: 97, type: "Healer" },
-    { name: "Haneen", level: 112, type: "Fighter" },
-  ];
-
+  ]);
+  
   return (
     <div className="mainContainer">
-      <Card info={boys} gender="boy" />
-      <Card info={girls} gender="girl" />
+      <Card info={boyState} gender="boy" deleteHandler={deleteHandler} />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,18 @@
 import image from "../assets/pfp.jpg";
-const Card = ({ info, gender }) => {
+const Card = ({ info, deleteHandler }) => {
   const cards = info.map(({ name, level, type }, idx) => (
-    <div
-      key={idx}
-      className="cardWrapper"
-      style={
-        gender === "boy"
-          ? { backgroundColor: "rgb(178 178 255)" }
-          : { backgroundColor: "pink" }
-      }
-    >
+    <div key={idx} className="cardWrapper">
       <div className="profileImage">
         <img src={image} />
       </div>
       <div className="name">Name: {name}</div>
       <div className="level">Level: {level}</div>
       <div className="class">Class: {type}</div>
+      <div className="btn" onClick={(e) => deleteHandler(idx)}>
+        X
+      </div>
     </div>
   ));
-  return <div>{cards}</div>;
+  return <>{cards}</>;
 };
 export default Card;
